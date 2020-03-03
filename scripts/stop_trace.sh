12bd2a61e8404f863ce7b00cbb9500092e7b9eb2
Alex Hoffman <alxhoff@gmail.com>
2020-03-03 18:11:50 +0100

    Added script argument to stop_trace.sh

--- scripts/stop_trace.sh
+++ scripts/stop_trace.sh
@@ -4,7 +4,8 @@ echo "Stopping sys_logger ..."
 adb shell "/data/local/tmp/sys_logger.sh stop"
 
 echo "Write the trace file ..."
-adb shell "/data/local/tmp/sys_logger.sh finish"
+adb shell "/data/local/tmp/sys_logger.sh finish $@"
 
 echo "Fetching the trace file ..."
 adb pull "/data/local/tmp/trace.dat" .
+adb pull "/data/local/tmp/trace.report" .
